c6ff969ca8f236bc42cb5f00bdf29fe3349b17d0
Sean Wills <swills044@gmail.com>
2017-09-15 20:59:59 +0100

    User does not display in link user

--- public/webapp/projects/project.controller.js
+++ public/webapp/projects/project.controller.js
@@ -2,47 +2,42 @@
     var module = angular.module('todoApp');
 
     module.controller('projectController', projectController);
-    module.filter("filterArrayByBlacklist", filter);
 
-    function filter($stateParams) {
-      var localid = localStorage.getItem('UserId')
-      var blackList = localid;
-      return function(array) {
-          return array.filter(function(item) {
-              return blackList.indexOf(item.id) === -1; // item id not found in blacklist
-          });
-      };
-    }
-    projectController.$inject = ['$scope','$http', '$window', 'project', 'tasks',  'projectusers', 'users', '$state'];
 
-    function projectController($scope, $http, $window, project, tasks, projectusers, users, $state) {
+    projectController.$inject = ['$scope','$http', '$window', 'project', 'tasks',  'projectusers', 'users', '$state', 'projectService'];
+
+    function projectController($scope, $http, $window, project, tasks, projectusers, users, $state, projectService) {
         $scope.localid = localStorage.getItem('UserId')
         $scope.project = project;
         $scope.tasks = tasks;
         $scope.projectusers = projectusers;
-        $scope.users = users;
+        var newUsers = [];
+        var localid = localStorage.getItem('UserId')
+        users.forEach(function(u){
+          if (u._id != localid){
+            debugger;
+            newUsers.push(u);
+            console.log(u)
+          }
+        });
+        $scope.users = newUsers;
         $scope.user = {};
-        $scope.form = false;
-        $scope.showform = function(){
-        	if ($scope.form == true) {
-        		$scope.form = false
-        	}else{
-        		$scope.form = true;
-        	}
 
+        $scope.editStatus = function(id, body){
+          projectService.editproject(id, body);
         }
 
         $('.open-datetimepicker').click(function(event){
 		    event.preventDefault();
 		    $('#datepicker').focus();
-		});
+    		});
 
-		$(function () {
-			$( "#datepicker" ).datepicker({
-				changeMonth: true,//this option for allowing user to select month
-				changeYear: true //this option for allowing user to select from year range
-			});
-		})
+    		$(function () {
+    			$( "#datepicker" ).datepicker({
+    				changeMonth: true,//this option for allowing user to select month
+    				changeYear: true //this option for allowing user to select from year range
+    			});
+    		})
 
 	    $scope.newTask = function(){
 			var data = {Name: $scope.name, Project: $scope.project._id ,DueDate: $scope.duedate};
--- public/webapp/services/projectService.js
+++ public/webapp/services/projectService.js
@@ -12,7 +12,7 @@
 		        method : "GET",
 		        url : window.endpoint + 'api/project',
 		        headers: {'Authorization': 'Bearer ' + localStorage.getItem("tokenString")}
-		    });	    
+		    });
 
 		};
 	//GetById
@@ -36,6 +36,16 @@
 			return $http({
 				method: 'GET',
 				url: window.endpoint + 'userprojects/' + id.id,
+				headers: {'Authorization': 'Bearer ' + localStorage.getItem("tokenString")}
+
+			})
+		}
+	//Edit
+		service.editproject = function(id, body){
+			return $http({
+				method: 'GET',
+				url: window.endpoint + 'project/' + id.id,
+				data: {'Status': body},
 				headers: {'Authorization': 'Bearer ' + localStorage.getItem("tokenString")}
 
 			})
